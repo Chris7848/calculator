c1e392c5434a2c7bd0edad10e5966082375c6e2f
Chris7848 <142909227+Chris7848@users.noreply.github.com>
2026-05-26 23:21:22 +0100

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -5,7 +5,7 @@ import time
 # =========================
 # BACKEND URL
 # =========================
-API_URL = "http://localhost:10000/run"
+API_URL = "https://calculator-1-8alq.onrender.com"
 
 st.set_page_config(page_title="VHDL ALU Simulator", layout="centered")
 
